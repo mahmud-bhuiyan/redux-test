1f47af3c6eb5491d823e044007c1f7c26b5fcca3
mahmud-bhuiyan <bh.mahmud@gmail.com>
2024-02-20 00:32:52 +0600

    redux implemented

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
 const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+
   return (
-    <div>
-      <h3>Counter</h3>
+    <div className="bg-[#38419D] p-14 rounded text-white flex gap-4">
+      <button className="p-2 bg-red-500 rounded">Decrement</button>
+      <div className="p-2">
+        <h1>{count}</h1>
+      </div>
+      <button className="p-2 bg-green-400 rounded">Increment</button>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Counter from "../components/Counter";
+
 const Home = () => {
   return (
-    <div>
-      <h3>Home</h3>
+    <div className="bg-[#3887BE] flex justify-center items-center h-screen">
+      <div>
+        <Counter />
+      </div>
     </div>
   );
 };
